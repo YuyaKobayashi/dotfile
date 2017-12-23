8880a648e123c0381a3eff7fd54cc56b6ac72602
YuyaKobayashi <ky.epk.leo@gmail.com>
2017-12-23 00:48:23 +0000

    small modification on install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,10 @@
 # Any modification in this repository is reflected on sourcing ./.bashrc.
 #
 
-user_bashrc=${HOME}/.bashrc
 script_dir=$(cd $(dirname $BASH_SOURCE); pwd)
+
+# for bash
+user_bashrc=${HOME}/.bashrc
 install_cmd="source $script_dir/.bashrc"
 if [[ ! -f $user_bashrc ]] || [[ -z "cat $user_bashrc | grep $install_cmd" ]]; then
 	echo $install_cmd >>$user_bashrc
