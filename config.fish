da1bcaca1d5ae924cf148a53b1b20733db6266b3
YuyaKobayashi <ky.epk.leo@gmail.com>
2017-12-23 01:30:13 +0000

    create config.fish

---
+++ config.fish
@@ -0,0 +1,44 @@
+#!/usr/bin/env fish
+alias rm="rm -i"
+alias py="python3"
+alias sc="screen"
+alias g="git"
+alias gi="git"
+alias l="ls --color"
+alias v="vim"
+alias grep="grep --color"
+
+# Initialization for git
+if [ -n "(command -s git)" ]
+	function set_undefined_gitconfig
+		set -l key $argv[1]
+		set -l val $argv[2]
+		if [ -z "(git config --get $key)" ]
+			git config --global $key "$val"
+		end
+	end
+	set_undefined_gitconfig alias.s status
+	set_undefined_gitconfig alias.b branch
+	set_undefined_gitconfig alias.a add
+	set_undefined_gitconfig alias.d diff
+	set_undefined_gitconfig alias.c "commit -v"
+	set_undefined_gitconfig alias.l "log --deco"
+	set_undefined_gitconfig merge.tool vimdiff
+	set_undefined_gitconfig color.ui true
+	set_undefined_gitconfig core.editor vim
+	set_undefined_gitconfig user.name YuyaKobayashi
+	set_undefined_gitconfig user.email ky.epk.leo@gmail.com
+
+	functions -e set_undefined_gitconfig
+end
+
+
+# Initialization for .vimrc
+set default_vimrc "$HOME/.vimrc"
+set script_dir (cd (dirname (status -f)); pwd)
+if [ -f "$default_vimrc" ]
+else
+	echo source $script_dir/.vimrc >$default_vimrc 
+end
+set -e default_vimrc
+set -e script_dir
