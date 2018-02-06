b0cad122a5ac59b3eb51981bfae8798e466a4ca4
YuyaKoabayshi <kobayashi.y.bk@m.titech.ac.jp>
2018-02-06 15:07:34 +0900

    change: do not show git status on prompt

--- config.fish
+++ config.fish
@@ -69,10 +69,10 @@ set red (set_color red)
 set gray (set_color -o black)
 
 ## fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showupstream 'yes'
+#set __fish_git_prompt_showdirtystate 'yes'
+#set __fish_git_prompt_showstashstate 'yes'
+#set __fish_git_prompt_showuntrackedfiles 'yes'
+#set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
