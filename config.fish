579f26f028c6d8bfc759fabefc771f032234ec44
Yuya Kobayashi <kobayashi.y.bk@m.titech.ac.jp>
2018-02-06 15:19:23 +0900

    change: change prompt color on PWD

--- config.fish
+++ config.fish
@@ -88,11 +88,12 @@ function fish_prompt
 	set last_status $status
 
 	set_color $fish_color_cwd
+	printf '%s@%s ' $USER (hostname -s)
 	
-	printf '%s@%s %s' $USER (hostname -s) (prompt_pwd)
+	set_color blue
+	printf '%s' (prompt_pwd)
 
 	set_color normal
-
 	printf '%s> ' (__fish_git_prompt)
 
 	set_color normal
