192b1fc83f43afeb3b8de083b2e5847f62e70dd2
YuyaKoabayshi <kobayashi.y.bk@m.titech.ac.jp>
2018-02-06 15:06:15 +0900

    add: on fish shell, git branch information is shown in prompt

--- config.fish
+++ config.fish
@@ -59,3 +59,41 @@ cd ~
 set -e script_dir
 set -e os
 
+
+# fish prompt
+set normal (set_color normal)
+set magenta (set_color magenta)
+set yellow (set_color yellow)
+set green (set_color green)
+set red (set_color red)
+set gray (set_color -o black)
+
+## fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+set __fish_git_prompt_char_dirtystate 'd'
+set __fish_git_prompt_char_stagedstate 's'
+set __fish_git_prompt_char_untrackedfiles 'z'
+set __fish_git_prompt_char_stashstate '*'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
+function fish_prompt
+	set last_status $status
+
+	set_color $fish_color_cwd
+	
+	printf '%s@%s %s' $USER (hostname -s) (prompt_pwd)
+
+	set_color normal
+
+	printf '%s> ' (__fish_git_prompt)
+
+	set_color normal
+end
