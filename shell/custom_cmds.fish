25a457000784366a0be0c3a3646f858fb83da1b8
yuyakobayashi98889 <yuya.kobayashi@dmprof.com>
2022-06-23 11:01:08 +0900

    [Feature] impl default working directory in docker_bash command

--- shell/custom_cmds.fish
+++ shell/custom_cmds.fish
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 function docker_bash
+    if test -z $argv
+        echo """
+        Please give docker image name.
+
+        [Usage] docker_bash [DOCKER_OPTIONS] IMAGE_NAME
+        """
+        return
+    end
     set user_opt -u $USER
+    set working_opt -w $PWD
     for opt in "$argv"
         if test $opt = "-u"
             set user_opt ""
         end
+        if test $opt = "-w"
+            set working_opt ""
+        end
     end
 
-    docker run --rm -it -v /home:/home $user_opt $argv bash
+    docker run --rm -it -v /home:/home $user_opt $working_opt $argv bash
 end
--- shell/custom_cmds.sh
+++ shell/custom_cmds.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
 docker_bash() {
+    if [[ -z "$1" ]]; then
+        echo """
+        Please give docker image name.
+
+        [Usage] docker_bash [DOCKER_OPTIONS] IMAGE_NAME
+        """
+        return
+    fi
+
     user_opt="-u $USER"
+    working_opt="-w $PWD"
     for opt in "$@"; do
         if [[ $opt == "-u" ]]; then
             user_opt=""
         fi
+        if [[ $opt == "-w" ]]; then
+            working_opt=""
+        fi
     done
 
-    docker run --rm -it -v /home:/home $user_opt $@ bash
+    docker run --rm -it -v /home:/home $user_opt $working_opt $@ bash
 }
