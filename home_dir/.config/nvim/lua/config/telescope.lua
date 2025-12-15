0b5248cb0531602edf8e07fbb7b4d506bde0c6d4
YuyaKobayashi <ky.epk.leo@gmail.com>
2025-12-15 10:13:58 +0900

    [Feature] remap <leader>ff -> <leader>fp

--- home_dir/.config/nvim/lua/config/telescope.lua
+++ home_dir/.config/nvim/lua/config/telescope.lua
@@ -1,7 +1,7 @@
 local builtin = require("telescope.builtin")
 
 -- 検索、ジャンプ強化：基本マッピング例
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+vim.keymap.set("n", "<leader>fp", builtin.find_files, { desc = "Find files" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help" })
