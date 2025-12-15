79e309553db5302e7eedb3bf5437de1a22853a8d
YuyaKobayashi <ky.epk.leo@gmail.com>
2025-12-15 10:12:54 +0900

    [Fix] do not use `g` key for lsp-based jump

--- home_dir/.config/nvim/lua/config/lsp.lua
+++ home_dir/.config/nvim/lua/config/lsp.lua
@@ -10,12 +10,6 @@ local on_attach = function(_, bufnr)
     vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
   end
 
-  -- 定義/参照/実装/型定義
-  map("n", "gd", vim.lsp.buf.definition, "Go to Definition")
-  map("n", "gr", vim.lsp.buf.references, "References")
-  map("n", "gi", vim.lsp.buf.implementation, "Go to Implementation")
-  map("n", "gt", vim.lsp.buf.type_definition, "Type Definition")
-
   -- シンボル/コードアクション/リネーム
   map("n", "K", vim.lsp.buf.hover, "Hover")
   map("n", "<leader>ca", vim.lsp.buf.code_action, "Code Action")
